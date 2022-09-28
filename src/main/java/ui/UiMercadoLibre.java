47e025b07901455f77a9755cb343d6434d455303
Joaquin Edgar Alcon Ruiz <joaquinear21@gmail.com>
2022-09-28 09:21:23 -0400

    last changes

--- src/main/java/ui/UiMercadoLibre.java
+++ src/main/java/ui/UiMercadoLibre.java
@@ -6,9 +6,8 @@ public class UiMercadoLibre {
     public static By country = By.id("BO");
     public static By searchBar = By.id("cb1-edit");
     public static By resultSearch = By.xpath("(//h1)[1]");
-    public static By usedQuantity = By.xpath("//a[@aria-label='Usado']//span[@class='ui-search-filter-results-qty']");
-    public static By usedQuantityClick = By.xpath("//a[@aria-label='Usado']//span[1]']");
-    public static By newQuantity = By.xpath("//a[@aria-label='Nuevo']//span[@class='ui-search-filter-results-qty']");
+    public static By usedQuantity = By.xpath("//div/ul/li/a[@aria-label='Usado']/span[@class='ui-search-filter-results-qty shops__filter-results-qty']");
+    public static By newQuantity = By.xpath("//div/ul/li/a[@aria-label='Nuevo']/span[@class='ui-search-filter-results-qty shops__filter-results-qty']");
     public static By cacheBtn = By.xpath("//button[@class='cookie-consent-banner-opt-out__action cookie-consent-banner-opt-out__action--primary cookie-consent-banner-opt-out__action--key-accept']");
     public static By filterOption = By.xpath("//section/a/div/div");
 }
